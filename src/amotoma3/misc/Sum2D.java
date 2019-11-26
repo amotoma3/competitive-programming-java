d32cf6dad0cf20b162e503bf0d7b33e86949ff66
Masahiro Yamamoto <amotoma3@gmail.com>
2019-11-26 23:31:52 +0900

    fix

--- src/amotoma3/misc/Sum2D.java
+++ src/amotoma3/misc/Sum2D.java
@@ -30,11 +30,11 @@ public class Sum2D {
 
     public void init() {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 sum[i + 1][j + 1] += sum[i + 1][j];
             }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 sum[j + 1][i + 1] += sum[j][i + 1];
             }
